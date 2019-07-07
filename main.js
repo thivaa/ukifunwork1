e7adca7806c0d22b5b7f473f869b85204f615a99
thivaa <thivagar1998@gmail.com>
2019-07-08 02:38:55 +0530

    songs added

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 $(document).ready(function() {
-  SC.stream('/tracks/568198284',function(sound){
+
+//rajani murukan yennamaa ippidi
+
+  SC.stream('/tracks/210606753',function(sound){
 
        $("#start").click(function(e) {
               e.preventDefault();
@@ -16,7 +19,10 @@ $(document).ready(function() {
                  sound.stop();
                });
 });
-SC.stream('/tracks/568198284',function(sound){
+
+//ngk peranppea
+
+SC.stream('/tracks/641888586',function(sound){
 
      $("#start1").click(function(e) {
             e.preventDefault();
@@ -29,5 +35,194 @@ SC.stream('/tracks/568198284',function(sound){
                sound.stop();
              });
 });
+
+
+//thupaki google
+
+
+SC.stream('/tracks/63007631',function(sound){
+
+     $("#start2").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop2").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+//maari2 mydear macha
+
+SC.stream('/tracks/556842441',function(sound){
+
+     $("#start3").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop3").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+//naanum rowdy thaan
+
+SC.stream('/tracks/225505838 ',function(sound){
+
+     $("#start4").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop4").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+//veeram nallavanu sollvaanka
+
+SC.stream('/tracks/125437193',function(sound){
+
+     $("#start5").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop5").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+//darmadurai aandipatti kanavaa
+
+SC.stream('/tracks/284005521',function(sound){
+
+     $("#start6").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop6").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+//iravukuAayiram naan mulusaa
+
+SC.stream('/tracks/397422978',function(sound){
+
+     $("#start7").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop7").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+//theeran sevatha pulla
+
+SC.stream('/tracks/349414693',function(sound){
+
+     $("#start8").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop8").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+// sethupathi konchipesida
+
+SC.stream('/tracks/303954855',function(sound){
+
+     $("#start9").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop9").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+//ispdae rajavum kannammaa unnai
+
+SC.stream('/tracks/602054046',function(sound){
+
+     $("#start10").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop10").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+//natpeathunai singlepasanka
+
+SC.stream('/tracks/584885949',function(sound){
+
+     $("#start11").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop11").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+
+
+
+
 
 });
